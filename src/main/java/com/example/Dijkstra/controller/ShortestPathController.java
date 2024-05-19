7e394e264fac4309b5c9bd054e470995e6725e01
23subbhasit <subbhashitmukherjee@gmail.com>
2024-05-19 20:09:05 +0530

    updated

--- src/main/java/com/example/Dijkstra/controller/ShortestPathController.java
+++ src/main/java/com/example/Dijkstra/controller/ShortestPathController.java
@@ -16,7 +16,7 @@ public class ShortestPathController {
         this.shortestPathService = shortestPathService;
     }
 
-    @GetMapping("/shortest-path")
+    @GetMapping("/dijkstra")
     public ShortestPathResult findShortestPath(
             @RequestParam("start") String start,
             @RequestParam("end") String end) {
